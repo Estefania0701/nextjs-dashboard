0d90cc26dab6b3b2babc07bc72e5da26d5a05448
Estefanía Aguas <estefania.aguas@puntored.co>
2025-05-30 13:13:19 -0500

    feat: create dashboard layout and pages for customers and invoices

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
+    /**
+     * RootLayout es el layout principal de la aplicación.
+     * Se requiere en toda aplicación Next.js.
+     * Cualquier UI que se añada a este archivo se aplicará a todas las páginas de la aplicación.
+     * Se puede usar para modificar las etiquetas html y body, y añadir metadatos.
+     */
     <html lang="en">
       <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
